31d2552efd2cece0a32ef137b4ad56360e2b983f
Dichenko Sergey <64374719+dsswork@users.noreply.github.com>
2021-09-22 11:09:42 +0300

    Add files via upload

---
+++ Internship/iOS/2021-KR/README.md
@@ -0,0 +1,11 @@
+Great application that transform input integer number into ordinal strings.
+
+Support English, Russian, German and Ukrainian language, transform numbers up to 10^15 - 1.
+
+Demo: https://number-to-string.000webhostapp.com/
+
+Developer Diachenko Sergey:
+https://www.facebook.com/diachenko.sergey
++380678390757
+
+
---
+++ Internship/iOS/2021-KR/storage/framework/views/e05e066c306da8dcb797a4fb23c5e11649942cf2.php
@@ -0,0 +1,66 @@
+<main class="form-signin" style="justify-content: center; display: flex;">
+    <div class="form-floating" style="text-align: center; width: 400px; margin-top: 300px">
+        <div class="form-group" style="vertical-align: center; height: 500px">
+            <div style="text-align: center">
+                <label id="mainLabel" class="form-label" style="font-size: 28px"><?php echo e(__('numbers.label')); ?></label><br>
+            </div>
+            <div class="">
+                <input id="number" class="form-control phoneClass" name="number" maxlength="15" style="border-radius: 4px"
+                       onkeyup="showNumber()">
+            </div>
+            <div class="form-group" style="margin-top: 20px">
+                <select id="selectLanguageButton" class="form-select" onchange="showNumber()">
+                    <option id="en" value="en">English</option>
+                    <option id="ru" value="ru">Русский</option>
+                    <option id="ua" value="ua">Украинский</option>
+                    <option id="de" value="de">Deutch</option>
+                </select>
+            </div>
+            <div class="">
+                <textarea id="result" class="form-control"
+                          name="result" style="border: none; margin-top: 20px; height: 300px; resize: none; background-color: white"
+                          readonly></textarea>
+            </div>
+        </div>
+    </div>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+
+        function showNumber() {
+            var number = $('#number').val();
+            let lang = $('#selectLanguageButton').val();
+            $.ajax({
+                url: "<?php echo e(route('number')); ?>",
+                method: 'POST',
+                data: {
+                    "_token": "<?php echo e(csrf_token()); ?>",
+                    "number": number,
+                    "lang": lang
+                },
+                success: function (data) {
+                    if($('#number').val().length==0) {
+                        $('#result').val('');
+                    } else {
+                        $('#result').val(data);
+                    }
+                }
+            });
+        }
+
+        <!-- Phone mask -->
+        $('body').on('keydown', '.phoneClass', function (e) {
+            var key = e.which || e.charCode || e.keyCode || 0;
+            $phone = $(this);
+
+            return (key == 8 ||
+                key == 9 ||
+                key == 46 || key == 37 ||
+                (key >= 48 && key <= 57) ||
+                (key >= 96 && key <= 105));
+        })
+    </script>
+</main>
+<?php /**PATH C:\OpenServer\domains\numbers\resources\views/number.blade.php ENDPATH**/ ?>
